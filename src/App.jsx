7d6cbca90bb61dc45476fd487ed55cd442d69f44
sanatan223 <sanatan_dev@outlook.com>
2025-09-17 10:45:36 +0530

    styled the pokemon card

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './poke-card.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='pokemon-card'>
+        <img className='pokemon-img' src="https://s3.pokeos.com/pokeos-uploads/assets/pokemon/home/render/376.png" alt="pokemon image" />
+        <div className="pokemon-data">
+          <div>
+            <p>Name: Probopass</p>
+            <p>Type: Rock/Steel</p>
+          </div>
+          <div>
+            <p>Gender: Male</p>
+            <p>Catch: 23%</p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
